b53a3f3786baf4623a5ff9509f80ea9c759e33df
ozonetw <ozonetw@gmail.com>
2015-10-13 20:18:22 -0500

    Primer prueba unitaria con mappeo de URL y vista falsa

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,5 +6,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'lists.views.home_page', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
 )
