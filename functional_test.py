8102b71673d8b76da1b54b07b7d861fdcd55b878
ozonetw <ozonetw@gmail.com>
2015-10-18 17:11:47 -0500

    La prueba funcional ahora busca que se puedan ingresar items a la lista

--- functional_test.py
+++ functional_test.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        #self.browser.implicity_wait(3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -17,10 +18,34 @@ class NewVisitorTest(unittest.TestCase):
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #She is invited to enter a to-do item straight away
-        #[...rest of comments as before]
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #She buys "Peacock flowers"
+        inputbox.send_keys('Buy Peacock feathers')
+
+        #When she hits enter the page updates and lists the stuff
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy Peacock feathers' for row in rows)
+        )
+
+        #Textbox inviting another item
+        self.fail('finish the test!')
+
+        #The page updates and shows the lists
+
+
 
 if __name__== '__main__':
     unittest.main(warnings='ignore')
