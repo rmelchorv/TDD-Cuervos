8e2aba0a476267734c258207b85e9c6d2eaef0b2
rmelchorv <rmelchorv@gmail.com>
2015-10-19 15:26:35 -0500

    Refactor and triangulation with indentation

--- functional_test.py
+++ functional_test.py
@@ -5,52 +5,51 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-    def tearDown(self):
-        self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        #Edith has heard about a cool new online to-do app. She goes
-        #to check out its homepage
-        self.browser.get('http://localhost:8000')
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
 
-        #She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
-        #She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        #self.assertEqual(
-        #    inputbox.get_attribute('placeholder'),
-        #    'Enter a to-do item'
-        #)
+		# She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		#self.assertEqual(
+		#    inputbox.get_attribute('placeholder'),
+		#    'Enter a to-do item'
+		#)
 
-        #She buys "Peacock flowers"
-        #inputbox.send_keys('Buy Peacock feathers')
-        inputbox.send_keys('Use peacock feathers to make a fly')
+		# She buys "Peacock flowers"
+		#inputbox.send_keys('Buy Peacock feathers')
+		inputbox.send_keys('Use peacock feathers to make a fly')
 
-        #When she hits enter the page updates, and now the page lists
-	#"1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)
+		# When she hits enter the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list table
+		inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy Peacock feathers', [row.text for row in rows])
-        self.assertIn(
-		'2: Use peacock feathers to make a fly' ,
-		[row.text for row in rows]
-	)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy Peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly' ,
+			[row.text for row in rows]
+		)
 
-        #Textbox inviting another item
-        self.fail('Finish the test!')
+		#Textbox inviting another item
+		self.fail('Finish the test!')
 
-        #The page updates and shows the lists
-
+		#The page updates and shows the lists
 
 
 if __name__== '__main__':
-    unittest.main(warnings='ignore')
+	unittest.main(warnings='ignore')
