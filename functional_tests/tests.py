24fc836e66ccc8f71dcb6b7cf376b49fbbadd8c6
rmelchorv <rmelchorv@gmail.com>
2015-10-20 15:56:38 -0500

    Uso de Bootstrap para mejorar el layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -23,37 +23,37 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to-do lists
-		### self.assertIn('To-Do', self.browser.title)
+		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		### self.assertIn('To-Do', header_text)
+		self.assertIn('To-Do', header_text)
 
 		# She is invited to enter a to-do item straight away
-		### inputbox = self.browser.find_element_by_id('id_new_item')
-		### inputbox.send_keys('Buy peacock feathers')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
 
 		# When she hits enter, she is taken to a new URL,
 		# and now the page list "1: Buy peacock feathers" as an item in a
 		# to-do list table
-		### inputbox.send_keys(Keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
 		edith_list_url = self.browser.current_url
-		### self.assertRegex(edith_list_url, '/lists/.+')
-		### self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.assertRegex(edith_list_url, '/lists/.+')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "User peacock feathers to make a fly" (Edith is very
 		# methodical)
-		### inputbox = self.browser.find_element_by_id('id_new_item')
-		### inputbox.send_keys('Use peacock feathers to make a fly')
-		### inputbox.send_keys(Keys.ENTER)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both items on her list
-		### self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-		### self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# Now a new user, Francies, comes along to the site.
 
-		## We use a new browser session to meke sure that no information
-		## of Edith's is coming through from cookies etc #
+		# We use a new browser session to meke sure that no information
+		# of Edith's is coming through from cookies etc #
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 
@@ -66,19 +66,19 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# Francis starts a new listby entering a new item. He
 		# is less interesting than Edith...
-		### inputbox = self.browser.find_element_by_id('id_new_item')
-		### inputbox.send_keys('Buy milk')
-		### inputbox.send_keys(Keys.ENTER)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
 
 		# Francis get his own unique URL
 		francis_list_url = self.browser.current_url
-		### self.assertRegex(francis_list_url, '/lists/.+')
-		### self.assertNotEqual(francis_list_url, edith_list_url)
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
 
 		# Again, the is no trace of Edith's list
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertNotIn('Buy milk', page_text)
+		self.assertIn('Buy milk', page_text)
 
 		# Satisfied, she goes back to sleep
 
