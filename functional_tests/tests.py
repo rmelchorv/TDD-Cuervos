e61675e0791137039511d06f209e0d0ba62f2642
rmelchorv <rmelchorv@gmail.com>
2015-10-20 12:41:31 -0500

    Primeros pasos de Pruebas Funcionales (FT) para Layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -81,3 +81,19 @@ class NewVisitorTest(LiveServerTestCase):
 		#self.assertNotIn('Buy milk', page_text)
 
 		# Satisfied, she goes back to sleep
+
+	def test_layout_and_styling(self):
+		# Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		
+		# ###She notices the input box is nicely centered
+		# She starts a new list and sees the input is nicely
+		# centered there too
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('testing\n')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
